e0426af9073505bbc64e65dc48276718e4caebb5
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-06-28 21:27:23 -0700

    new set of function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,61 @@
-const vowels = ["a", "e", "i", "o", 'u"'];
+let word = "se7en";
 
-function vowelCounter(word, text) {
-    const textArray = text.split(" ");
-    for (let i = 0; i < textArray.length; i++) {
-        console.log(i);
-        if (word === textArray[i]) {
-            return i;
-        }
-    }
-    return -1;
+const vowelArray = ["a", "e", "i", "o", "u"];
+
+function isCharacterALetter(char) {
+    return (/[a-zA-Z]/).test(char);
 }
 
-//const vowels = ["a", "e", "i", "o", "u"];
+function cleanNonChars(word) {
+    word.forEach(function(character) {
+        if (!isCharacterALetter(character)) {
+            //console.log("CAUGHT YOU " + character);
+            word.splice(word.indexOf(character), 1);
+        }
+    });
+    return word;
+}
 
+function findVowel(vowelArray, word) {
+    let shiftedWord = [];
+    word = word.split("");
+    firstLetter = word[0];
+    word = cleanNonChars(word);
 
-function firstInstanceOfWord(word, text) {
-    const textArray = text.split(" ");
-    for (let i = 0; i < textArray.length; i++) {
-        console.log(i);
-        if (word === textArray[i]) {
-            return i;
+    for (let i = 0; i < vowelArray.length; i += 1) {
+        if (firstLetter.toLowerCase() === vowelArray[i]) {
+            return word + "way";
         }
     }
-    return -1;
+    let newWord = "";
+
+
+    const wordLength = word.length
+    if (word[0] === "q" && word[1] === "u") {
+        shiftedWord = word.splice(0, 2);
+    }
+
+    for (let i = 0; i < wordLength; i++) {
+        indexVar = 0;
+        firstLetter = word[0];
+
+        if (word[0] === "q" && word[1] === "u") {
+            indexVar += 2;
+        }
+        if (!vowelArray.includes(word[indexVar])) {
+            //       console.log(word[indexVar]);
+            let firstConsonant = word.shift();
+            shiftedWord.push(firstConsonant);
+        } else {
+            word = word.join("");
+            shiftedWord = shiftedWord.join("");
+            //  console.log("WORD: " + word + " SHIFTED WORD: " + shiftedWord);
+            return word + shiftedWord + "ay"; // want "eakbbbray"
+        }
+    }
 }
+
+
+newWord = findVowel(vowelArray, word);
+
+console.log(newWord);
